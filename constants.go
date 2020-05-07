19c32f4405f1dd6592ea79f3f74838681b65b021
magikitty <elina.satola@gmail.com>
2020-05-07 14:03:56 +0300

    Revert messageMenuInvalidInput

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 package menu
 
 // messageMenuInvalidInput if user did not press number corresponding to menu item
-const messageMenuInvalidInput = "Press the number corresponding to a menu item."
+const messageMenuInvalidInput = "Press the number of a menu item."
 
 // messageSelectionUser for showing the user what they selected
 const messageSelectionUser = "You selected:"
